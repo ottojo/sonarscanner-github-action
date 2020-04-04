f3f54fb3b6449026078d411e69ab1e7985c8ff3f
Jonas Otto <jottosmail@gmail.com>
2020-04-04 17:04:03 +0200

    add download, path debug

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
+const download = require('download');
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
+
+    console.log(`PATH is ${process.env.PATH}`);
+
+    const scannerVersion = core.getInput("sonar-scanner-version");
+    const scannerDownloadURL = "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-"
+        + scannerVersion + "-linux.zip";
+
+    console.log(`downloading sonar-scanner from ${scannerDownloadURL}`);
+    download(scannerDownloadURL, "/tmp/sonarscanner");
+    console.log("done");
+
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
